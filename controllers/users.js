722a516521d3307060e0e125e42251ede9b53bdf
AKeyframe <kagebro@gmail.com>
2021-10-22 18:15:16 -0500

    AUTH WORKING

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userRouter = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
+const secret = process.env.SECRET;
 
 userRouter.post('/signup', async (req, res) => {
     console.log('Signup req.body');
@@ -9,12 +12,48 @@ userRouter.post('/signup', async (req, res) => {
     const user = new User(req.body);
     try {
         await user.save();
+        //create the web token for auth
+        const token = createJWT(user);
+        res.json({token});
         
-        res.json(user);
     } catch (err) {
         
         res.status(400).json(err);
     }
 });
+
+userRouter.post('/login', async (req, res) => {
+    try{
+        console.log(req.body);
+        const user = await User.findOne({username: req.body.username});
+        console.log(user);
+        if(!user) res.status(401).json({err: 'Bad Credentials'});
+        console.log('about to compare');
+        console.log(req.body.pw);
+        console.log(user.password);
+        let isMatch = bcrypt.compareSync(req.body.pw, user.password)
+            console.log('after compare');
+            console.log(isMatch);
+            if(isMatch) {
+                const token = createJWT(user);
+                res.json({token});
+                
+            } else {
+                return res.status(401).json({err: 'Bad Credentials'});
+            }
+        
+    } catch (err) {
+        return res.status(401).json(err);
+    }
+})
+
+//////////////////////////////////////////////
+
+
+
+function createJWT(user) {
+    return jwt.sign({user}, secret, {expiresIn: '24h'});
+}
+
 
 module.exports = userRouter;
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
+const salt = 12;
 
 const userSchema = new Schema({
     email: {
@@ -16,12 +18,30 @@ const userSchema = new Schema({
   timestamps: true
 });
 
+userSchema.methods.comparePassword = function(tryPass, cb){
+   
+    bcrypt.compareSync(tryPass, this.passsword);
+}
+
 userSchema.set('toJSON', {
   transform: function (doc, ret) {
     // remove the password property when serializing doc to JSON
     delete ret.password;
     return ret;
   }
+});
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    //Check to see if the password has been changed
+    //if it hasn't return, otherwise hash and salt
+    if(!user.isModified('password')) return next();
+    bcrypt.hash(user.password, salt, (error, hash) => {
+        if(error) return next(error);
+        //replace user password with the hash
+        user.password = hash;
+        next();
+    });
 });
 
 const User = mongoose.model('User', userSchema);
