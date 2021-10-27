7c74c8cfb4b12d6388213c7f04068c2ba3ba7dd4
AKeyframe <kagebro@gmail.com>
2021-10-26 21:09:55 -0500

    show route

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -15,10 +15,19 @@ recipeRouter.get('/', async (req, res) => {
 
 //Create
 recipeRouter.post('/', async (req, res) => {
-  console.log('--------------------------');
     try {
-        console.log(req.body);
         res.json(await Recipe.create(req.body));
+    } catch (error) {
+        //bad request
+        res.status(400).json(error);
+    }
+});
+
+//Show
+recipeRouter.get('/:id', async (req, res) => {
+    console.log(req.body);
+    try {
+        res.json(await Recipe.findById(req.params.id));
     } catch (error) {
         //bad request
         res.status(400).json(error);
